02a94f19782c6bc5ea267c05c3f53502665c3388
Sulthan Abiyyu <zblackjack980@gmail.com>
2023-02-05 18:06:11 +0700

    init project

---
+++ setup.py
@@ -0,0 +1,15 @@
+
+from setuptools import setup
+
+setup(
+    name='ppm',
+    version='0.1',
+    description='Collection of from-scratch machine learning implementation for Basic Machine Learning course.',
+    author='Sulthan Abiyyu Hakim',
+    author_email='sabiyyuhakim@gmail.com',
+    packages=['ppm'],
+    install_requires=[
+        'numpy',
+        'pandas',
+    ],
+)
